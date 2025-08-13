09ebbed4837c9b93348df77ee8d63effcbc83c1b
Dariusz Bielik <bdarek@gmail.com>
2025-08-12 20:51:18 -0400

    Updated Rclone options - set for 8 transfers

--- ezclon.sh
+++ ezclon.sh
@@ -1,15 +1,5 @@
-!/bin/bash
+#!/bin/bash
 
-USAGE="Usage: $0 [-d|--delete] source destination"
-
-OPT=""
-
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -d) OPT="--delete"; shift ;;
-        *) break ;;
-    esac
-done
 
 if [ "$#" -ne 2 ]; then
     echo "$USAGE"
@@ -30,10 +20,10 @@ find "$src" -maxdepth 1 -mindepth 1 -type d ! -name "restor*" -print0 | while IF
   echo $rsync_dest
 
   run_rclone() {
-    rclone sync "$rsync_src" "$rsync_dest" --metadata --checksum --progress --copy-links --no-update-modtime
+    rclone sync "$rsync_src" "$rsync_dest" --transfers 8 --progress --links --no-update-modtime
   }
 
-  while [[ $(pgrep -f "rclone" | wc -l) -ge 50 ]]; do
+  while [[ $(pgrep -f "rclone" | wc -l) -ge 100 ]]; do
     sleep 10  # Wait
   done
 
